e08f9c2362c65a4b884df8739de8379a2a2b41b3
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-08-23 18:56:17 +0530

    continuous sum query

--- advance/ContinuousSumQuery.java
+++ advance/ContinuousSumQuery.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
 Problem Description
 There are A beggars sitting in a row outside a temple.
@@ -41,5 +44,81 @@ Second devotee donated 20 coins to beggars ranging from 2 to 3. Final amount in 
 Third devotee donated 25 coins to beggars ranging from 2 to 5. Final amount in each beggars pot after third devotee: [10, 55, 45, 25, 25]
 
  */
-public class ContinousSumQuery {
+public class ContinuousSumQuery {
+
+    public static void main (String args []){
+
+        int A = 5;
+        ArrayList<ArrayList<Integer>> Q = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(Arrays.asList(1, 2, 10)),
+                        new ArrayList<>(Arrays.asList(2, 3, 20)),
+                        new ArrayList<>(Arrays.asList(2, 5, 25))
+                )
+        );
+        int n = Q.size();
+
+        bruteForce(A, Q, n);
+
+        optimized(A, Q,n);
+
+    }
+
+    public static void bruteForce(int A, ArrayList<ArrayList<Integer>> Q, int n){
+
+        ArrayList<Integer> a = new ArrayList<>(A);
+
+        for(int i = 0; i < A; i++){
+            a.add(0);
+        }
+
+        for(int i = 0; i < n; i++){
+
+            int left = Q.get(i).get(0) - 1;
+            int right = Q.get(i).get(1);
+            int val = Q.get(i).get(2);
+
+
+            for(int j = left; j < right; j++){
+                a.set(j, a.get(j) + val);
+            }
+
+        }
+
+        System.out.println(a);
+
+        // Time O(Q * N);
+        // Space O(N);
+    }
+    public static void optimized(int A, ArrayList<ArrayList<Integer>> B, int n){
+
+        ArrayList<Integer> a = new ArrayList<>(A);
+
+        for(int i = 0; i < A; i++){
+            a.add(0);
+        }
+
+        for(int i = 0; i < B.size(); i++){
+
+            int start = B.get(i).get(0) - 1;
+            int end = B.get(i).get(1);
+            int val = B.get(i).get(2);
+
+            a.set(start, a.get(start) + val);
+
+            if(end < a.size()){
+                a.set(end, a.get(end) - val);
+            }
+
+        }
+
+        for(int i = 1; i < a.size(); i++){
+            a.set(i, a.get(i - 1) + a.get(i));
+        }
+
+        System.out.println(a);
+
+        // Time O(Q + N);
+        // Space O(N);
+    }
 }
