2c3881563e3f6d2e9163c11a3e223a0a188f1a07
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-08-23 13:26:29 +0530

    kadane's algorightm

---
+++ advance/maxSumContiguousSubArray.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Problem Description
+Find the maximum sum of contiguous non-empty subarray within an array A of length N.
+
+Problem Constraints
+1 <= N <= 1e6
+-1000 <= A[i] <= 1000
+
+Input Format
+The first and the only argument contains an integer array, A.
+
+Output Format
+Return an integer representing the maximum possible sum of the contiguous subarray.
+
+Example Input
+
+Input 1: A = [1, 2, 3, 4, -10]
+Input 2: A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+Example Output
+Output 1: 10
+Output 2: 6
+
+Example Explanation
+
+Explanation 1:
+ The subarray [1, 2, 3, 4] has the maximum possible sum of 10.
+
+Explanation 2:
+ The subarray [4,-1,2,1] has the maximum possible sum of 6.
+
+ */
+public class maxSumContiguousSubArray {
+
+    public static void main (String args []){
+
+
+        List<Integer> A = new ArrayList<>(Arrays.asList(-2, 1, -3, 4, -1, 2, 1, -5, 4));
+
+        int n = A.size();
+
+        bruteForce(A, n);
+
+        kadanesAlgorithm(A, n);
+    }
+
+    public static void bruteForce (List<Integer> A, int n ){
+
+        int maxSum = A.get(0);
+
+        for( int i = 0; i < n; i++){
+
+            int sum = 0;
+
+            for(int j = i; j < n; j++){
+
+                sum+= A.get(j);
+
+                if(sum > maxSum) maxSum =sum;
+
+            }
+        }
+
+        System.out.println("maxSum : " + maxSum);
+
+        // Time O(N^2);
+        // Space O(1);
+    }
+
+    public static void kadanesAlgorithm (List<Integer> A, int n){
+
+        int maxSum = A.get(0);
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum+= A.get(i);
+
+            if(sum > maxSum) maxSum = sum;
+
+            if(sum < 0) sum = 0;
+        }
+
+        System.out.println("maxSum : " + maxSum);
+
+    }
+
+}
