10e066b2135a39a950a4e7cf78d88cdfc98f6cab
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-09-05 05:43:46 +0530

    Recursion One

--- advance/recursion/DecreasingIncreasingInOneFunction.java
+++ advance/recursion/DecreasingIncreasingInOneFunction.java
@@ -1,2 +1,52 @@
-package recursion;public class DecreasingIncreasingInOneFunction {
+package recursion;
+
+/*
+Problem Description
+Print N numbers in Decreasing Order and then in Increasing Order.
+    You are given a positive number N.
+    You are required to print the numbers from N to 1.
+    You are required to not use any loops. Don't change the signature of the function DecThenInc function.
+Note : Please print an new line after printing the output.
+
+Problem Constraints
+1 <= N <= 100
+
+Input Format
+The first line contains a single integer N.
+
+Output Format
+A single line having number printed from N to 1 and then from 1 to N.
+
+Example Input
+Input 2: 1
+Input 1: 4
+
+Example Output
+Output 1:
+1 1
+
+Output 2:
+4 3 2 1 1 2 3 4
+ */
+public class DecreasingIncreasingInOneFunction {
+
+    public static  void recursion(int A){
+
+        if(A == 1) {
+            System.out.print(1 + " " + 1 + " ");
+            return;
+        }
+
+        System.out.print(A + " ");
+        recursion(A - 1);
+        System.out.print(A + " ");
+    }
+    public static void main (String args []){
+
+        int A = 3;
+
+
+        recursion(A);
+        System.out.println();
+    }
 }
--- advance/recursion/SumOfDigits.java
+++ advance/recursion/SumOfDigits.java
@@ -1,2 +1,49 @@
-package recursion;public class SumOfDigits {
+package recursion;
+
+/*
+Problem Description
+Given a number A, we need to find the sum of its digits using recursion.
+
+Problem Constraints
+1 <= A <= 10^9
+
+Input Format
+The first and only argument is an integer A.
+
+Output Format
+Return an integer denoting the sum of digits of the number A.
+
+Example Input
+Input 1: A = 46
+Input 2: A = 11
+
+Example Output
+Output 1: 10
+Output 2: 2
+
+Example Explanation
+Explanation 1:
+ Sum of digits of 46 = 4 + 6 = 10
+
+Explanation 2:
+ Sum of digits of 11 = 1 + 1 = 2
+ */
+public class SumOfDigits {
+
+    public static int recursion(int A, int ans){
+        if(A == 0) return ans;
+
+        ans+= (int) (A%10);
+        return recursion((int) A/10, ans);
+    }
+    public static void main (String args []){
+
+        int A = 123;
+        int ans = 0;
+
+        int val = recursion(A, ans);
+
+        System.out.println(val);
+    }
+
 }
