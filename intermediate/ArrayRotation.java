83fdb96e6f64969391e3661b9e24b7c69fbbcb7a
Sarang Kaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2024-11-11 12:23:03 +0530

    Update ArrayRotation.java

--- intermediate/ArrayRotation.java
+++ intermediate/ArrayRotation.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
         you have to return the same array after rotating it B times towards the right.
 
     Problem Constraints
-        1 <= N <= 105
-        1 <= A[i] <=109
-        1 <= B <= 109
+        1 <= N <= 10^5
+        1 <= A[i] <=10^9
+        1 <= B <= 10^9
 
     Input Format
         The first argument given is the integer array A.
