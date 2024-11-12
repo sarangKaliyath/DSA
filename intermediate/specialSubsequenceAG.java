f8018fdd6c733b58e973e49faee6e69031799618
Sarang Kaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2024-11-12 18:46:20 +0530

    Update specialSubsequenceAG.java

--- intermediate/specialSubsequenceAG.java
+++ intermediate/specialSubsequenceAG.java
@@ -4,7 +4,7 @@ Problem Description
 You have given a string A having Uppercase English letters.
 You have to find the number of pairs (i, j) such that A[i] = 'A', A[j] = 'G' and i < j.
 
-Problem Constraints 1 <= length(A) <= 105
+Problem Constraints 1 <= length(A) <= 10^5
 
 Input Format
 First and only argument is a string A.
