84e16ba84a47bd65fa2d0093324fb5a14040ed47
Sarang Kaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2024-11-15 04:40:04 +0530

    Update majorityElement.java

--- intermediate/majorityElement.java
+++ intermediate/majorityElement.java
@@ -10,8 +10,8 @@ The majority element is the element that appears more than floor(n/2) times.
 You may assume that the array is non-empty and the majority element always exists in the array.
 
 Problem Constraints
-1 <= N <= 5*105
-1 <= num[i] <= 109
+1 <= N <= 5*10^5
+1 <= num[i] <= 10^9
 
 Input Format
 Only argument is an integer array.
