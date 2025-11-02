8e555ee27ea6973b10fc85e680b8e91f1c6842ff
Sarang Kaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2025-11-02 09:05:12 +0530

    Update addBinaryStrings.java

--- intermediate/addBinaryStrings.java
+++ intermediate/addBinaryStrings.java
@@ -66,6 +66,36 @@ public class addBinaryStrings {
 
     }
 
+    public String addBinary(String A, String B) {
+	    int nA, nB;
+	    char res[];
+	    int i, j, k;
+	    nA = A.length();
+	    nB = B.length();
+	    res = new char[Math.max(nA, nB) + 1];
+	    k = Math.max(nA, nB);
+	    i = nA - 1;
+	    j = nB - 1;
+	    int sum = 0, carry = 0;
+	    // we add bits from the rightmost bit to the leftmost bit
+	    while (i >= 0 || j >= 0 || carry != 0) {
+	        sum = carry;
+	        if (i >= 0)
+	            sum += (A.charAt(i) - '0');
+	        if (j >= 0)
+	            sum += (B.charAt(j) - '0');
+	        res[k] = (char) ((sum % 2) + '0');
+	        carry = sum / 2;
+	        i--;
+	        j--;
+	        k--;
+	    }
+	    if (res[0] == '1')
+	        return new String(res);
+	    int len = Math.max(nA, nB);
+	    return new String(res, 1, len);
+	}
+
     public static long binaryToDecimal(String str, int n) {
 
         long[] arr = new long[n];
